08689b25594fb1fed1bd0ebf2ff544b67c34e023
Andre Walker <andre.walker@booking.com>
2020-08-24 18:25:56 +0200

    Run ormolu

--- src/Main.hs
+++ src/Main.hs
@@ -3,220 +3,233 @@
 
 module Main where
 
-import Data.Time.Clock.POSIX
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.Map as M
 import qualified Data.Text as DT
+import Data.Time.Clock.POSIX
+import Hakyll
+import Portuguese
+import System.FilePath
 import qualified Text.DocTemplates as DocT
 import Text.Pandoc
 import Text.Pandoc.PDF (makePDF)
-import System.FilePath
-import Portuguese
-import Hakyll
 
 main :: IO ()
 main = hakyllWith config rules
 
 config :: Configuration
-config = defaultConfiguration {
-    deployCommand = "s3_website push"
-}
+config =
+  defaultConfiguration
+    { deployCommand = "s3_website push"
+    }
 
 rules :: Rules ()
 rules = do
-    staticId <- preprocess getStaticId
-    rulesWithStaticId staticId
+  staticId <- preprocess getStaticId
+  rulesWithStaticId staticId
 
 rulesWithStaticId :: Integer -> Rules ()
 rulesWithStaticId staticId = do
-    match "keybase.txt" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "keybase.txt" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "assets/webicons/*" $ do
-        route $ assetsRoute' "webicons/"
-        compile copyFileCompiler
+  match "assets/webicons/*" $ do
+    route $ assetsRoute' "webicons/"
+    compile copyFileCompiler
 
-    match "assets/fonts/*" $ do
-        route $ assetsRoute' "fonts/"
-        compile copyFileCompiler
+  match "assets/fonts/*" $ do
+    route $ assetsRoute' "fonts/"
+    compile copyFileCompiler
 
-    match "assets/js/*" $ do
-        route $ assetsRoute' "js/"
-        compile copyFileCompiler
+  match "assets/js/*" $ do
+    route $ assetsRoute' "js/"
+    compile copyFileCompiler
 
-    match "assets/css/*" $ do
-        -- Uncomment the following line for debugging:
-        -- route idRoute
-        -- And access at http://localhost:8000/assets/css
-        compile $ getResourceBody >>= saveSnapshot "raw"
+  match "assets/css/*" $ do
+    -- Uncomment the following line for debugging:
+    -- route idRoute
+    -- And access at http://localhost:8000/assets/css
+    compile $ getResourceBody >>= saveSnapshot "raw"
 
-    create ["style.css"] $ do
-        route $ assetsRoute' ""
-        compile $ do
-            items <- loadAllSnapshots "assets/css/*" "raw"
-            makeItem $ compressCss $ concat $ map itemBody (items :: [Item String])
+  create ["style.css"] $ do
+    route $ assetsRoute' ""
+    compile $ do
+      items <- loadAllSnapshots "assets/css/*" "raw"
+      makeItem $ compressCss $ concat $ map itemBody (items :: [Item String])
 
-    match "cv.md" $ do
-        route   $ setExtension "pdf"
-        compile pdfCompiler
+  match "cv.md" $ do
+    route $ setExtension "pdf"
+    compile pdfCompiler
 
-    match "pages/*" $ do
-        route   pagesRoute
-        compile $ do
-            pandocCompiler
-                >>= loadAndApplyTemplate "templates/page.html"    siteCtx'
-                >>= loadAndApplyTemplate "templates/default.html" siteCtx'
-                >>= relativizeUrls
+  match "pages/*" $ do
+    route pagesRoute
+    compile $ do
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/page.html" siteCtx'
+        >>= loadAndApplyTemplate "templates/default.html" siteCtx'
+        >>= relativizeUrls
 
-    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+  tags <- buildTags "posts/*" (fromCapture "tags/*.html")
 
-    tagsRules tags $ \tag pattern -> do
-        let title = "Posts com a tag \"" ++ tag ++ "\""
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll pattern
-            let ctx = mconcat
-                  [ constField "title" title
-                  , listField "posts" postCtx' (return posts)
-                  , siteCtx'
-                  ]
+  tagsRules tags $ \tag pattern -> do
+    let title = "Posts com a tag \"" ++ tag ++ "\""
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pattern
+      let ctx =
+            mconcat
+              [ constField "title" title,
+                listField "posts" postCtx' (return posts),
+                siteCtx'
+              ]
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/tag.html" ctx
-                >>= loadAndApplyTemplate "templates/default.html" ctx
-                >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/tag.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ do
-            let postCtxWithTags = tagsField "tags" tags <> postCtx'
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ do
+      let postCtxWithTags = tagsField "tags" tags <> postCtx'
 
-            pandocCompiler
-                >>= saveSnapshot "content"
-                >>= loadAndApplyTemplate "templates/post.html"    postCtxWithTags
-                >>= loadAndApplyTemplate "templates/default.html" postCtxWithTags
-                >>= relativizeUrls
+      pandocCompiler
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/post.html" postCtxWithTags
+        >>= loadAndApplyTemplate "templates/default.html" postCtxWithTags
+        >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
 
-            let archiveCtx = mconcat
-                  [ listField "posts" postCtx' (return posts)
-                  , constField "title" "Arquivo"
-                  , field "tagcloud" (\_ -> renderTagCloud 100 250 (sortTagsBy caseInsensitiveTags tags))
-                  , siteCtx'
-                  ]
+      let archiveCtx =
+            mconcat
+              [ listField "posts" postCtx' (return posts),
+                constField "title" "Arquivo",
+                field "tagcloud" (\_ -> renderTagCloud 100 250 (sortTagsBy caseInsensitiveTags tags)),
+                siteCtx'
+              ]
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
 
-    create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = bodyField "description" <> postCtx'
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx = bodyField "description" <> postCtx'
 
-            posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+      posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
 
-            renderAtom feedConfiguration feedCtx posts
+      renderAtom feedConfiguration feedCtx posts
 
-    blog <- buildPaginateWith
-        (\ids -> return $ paginateEvery 10 $ reverse ids)
-        "posts/*"
-        (\n -> if n == 1
+  blog <-
+    buildPaginateWith
+      (\ids -> return $ paginateEvery 10 $ reverse ids)
+      "posts/*"
+      ( \n ->
+          if n == 1
             then "index.html"
-            else fromCapture "posts/page/*.html" (show n))
+            else fromCapture "posts/page/*.html" (show n)
+      )
 
-    paginateRules blog $ \pageNum pattern -> do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll pattern
+  paginateRules blog $ \pageNum pattern -> do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pattern
 
-            let paginateCtx = paginateContext blog pageNum
-            let ctx         = mconcat
-                  [ constField "title" "Home"
-                  , listField "posts" (postCtx' <> paginateCtx) (return posts)
-                  , paginateCtx
-                  , siteCtx'
-                  ]
+      let paginateCtx = paginateContext blog pageNum
+      let ctx =
+            mconcat
+              [ constField "title" "Home",
+                listField "posts" (postCtx' <> paginateCtx) (return posts),
+                paginateCtx,
+                siteCtx'
+              ]
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/index.html" ctx
-                >>= loadAndApplyTemplate "templates/default.html" ctx
-                >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/index.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
-
+  match "templates/*" $ compile templateCompiler
   where
     postCtx' = postCtx staticId
     siteCtx' = siteCtx staticId
     assetsRoute' = assetsRoute staticId
 
-
 pdfCompiler :: Compiler (Item LB.ByteString)
 pdfCompiler = do
-    content <- readPandoc =<< getResourceBody
+  content <- readPandoc =<< getResourceBody
 
-    res <- unsafeCompiler $ runIOorExplode $ do
-        templ <- compileDefaultTemplate (DT.pack "latex")
-        makePDF "pdflatex" [] writeLaTeX def
-           {
-              writerTemplate = Just templ,
-              writerVariables = DocT.Context $ M.fromList [(DT.pack "geometry", DocT.toVal $ DT.pack "margin=2cm")]
-           } (itemBody content)
+  res <- unsafeCompiler $
+    runIOorExplode $ do
+      templ <- compileDefaultTemplate (DT.pack "latex")
+      makePDF
+        "pdflatex"
+        []
+        writeLaTeX
+        def
+          { writerTemplate = Just templ,
+            writerVariables = DocT.Context $ M.fromList [(DT.pack "geometry", DocT.toVal $ DT.pack "margin=2cm")]
+          }
+        (itemBody content)
 
-    case res of
-        Right pdf -> makeItem pdf
-        Left  err -> error $ show err
+  case res of
+    Right pdf -> makeItem pdf
+    Left err -> error $ show err
 
 dateFieldLoc :: String -> String -> Context a
 dateFieldLoc = dateFieldWith timeLocalePtBr
 
 postCtx :: Integer -> Context String
-postCtx staticId = mconcat
-    [ teaserField "teaser" "content"
-    , dateFieldLoc "date"       "%e de %B, %Y"
-    , dateFieldLoc "date_full"  "%F"
-    , dateFieldLoc "date_dmy"   "%d/%m/%Y"
-    , dateFieldLoc "date_day"   "%d"
-    , dateFieldLoc "date_month" "%b"
-    , dateFieldLoc "date_year"  "%Y"
-    , siteCtx staticId
+postCtx staticId =
+  mconcat
+    [ teaserField "teaser" "content",
+      dateFieldLoc "date" "%e de %B, %Y",
+      dateFieldLoc "date_full" "%F",
+      dateFieldLoc "date_dmy" "%d/%m/%Y",
+      dateFieldLoc "date_day" "%d",
+      dateFieldLoc "date_month" "%b",
+      dateFieldLoc "date_year" "%Y",
+      siteCtx staticId
     ]
 
 siteCtx :: Integer -> Context String
-siteCtx staticId = mconcat
-    [ constField "staticId" (show staticId)
-    , defaultContext
+siteCtx staticId =
+  mconcat
+    [ constField "staticId" (show staticId),
+      defaultContext
     ]
 
 feedConfiguration :: FeedConfiguration
-feedConfiguration = FeedConfiguration
-    { feedTitle       = "andrewalker.net"
-    , feedDescription = "Lorem ipsum dolor sit amet"
-    , feedAuthorName  = "André Walker"
-    , feedAuthorEmail = "andre+website@andrewalker.net"
-    , feedRoot        = "https://andrewalker.net"
+feedConfiguration =
+  FeedConfiguration
+    { feedTitle = "andrewalker.net",
+      feedDescription = "Lorem ipsum dolor sit amet",
+      feedAuthorName = "André Walker",
+      feedAuthorEmail = "andre+website@andrewalker.net",
+      feedRoot = "https://andrewalker.net"
     }
 
 getStaticId :: IO Integer
 getStaticId = round `fmap` Data.Time.Clock.POSIX.getPOSIXTime
 
-assetsRoute ::  Integer -> String -> Routes
+assetsRoute :: Integer -> String -> Routes
 assetsRoute staticId folder = customRoute $ prefixPlus . takeFileName . toFilePath
   where
-    prefix     = (show staticId) ++ "/" ++ folder
+    prefix = (show staticId) ++ "/" ++ folder
     prefixPlus = (prefix ++)
 
 pagesRoute :: Routes
 pagesRoute =
-    gsubRoute "pages/" (const "") `composeRoutes`
-    setExtension "html"
+  gsubRoute "pages/" (const "")
+    `composeRoutes` setExtension "html"
--- src/Portuguese.hs
+++ src/Portuguese.hs
@@ -1,27 +1,40 @@
-module Portuguese (
-    timeLocalePtBr
-) where
+module Portuguese
+  ( timeLocalePtBr,
+  )
+where
 
 import Data.Time.Format
 
 timeLocalePtBr :: TimeLocale
-timeLocalePtBr = TimeLocale {
-        wDays  = [("Domingo",  "Dom"),  ("Segunda",   "Seg"),
-                  ("Terça",    "Ter"),  ("Quarta",    "Qua"),
-                  ("Quinta",   "Qui"),  ("Sexta",     "Sex"),
-                  ("Sábado",   "Sáb")],
-
-        months = [("Janeiro",   "Jan"), ("Fevereiro", "Fev"),
-                  ("Março",     "Mar"), ("Abril",     "Abr"),
-                  ("Maio",      "Mai"), ("Junho",     "Jun"),
-                  ("Julho",     "Jul"), ("Agosto",    "Ago"),
-                  ("Setembro",  "Set"), ("Outubro",   "Out"),
-                  ("Novembro",  "Nov"), ("Dezembro",  "Dez")],
-
-        amPm = ("am", "pm"),
-        dateTimeFmt = "%a %b %e %H:%M:%S %Z %Y",
-        dateFmt = "%d/%m/%y",
-        timeFmt = "%H:%M:%S",
-        time12Fmt = "%I:%M:%S %p"
-        }
-
+timeLocalePtBr =
+  TimeLocale
+    { wDays =
+        [ ("Domingo", "Dom"),
+          ("Segunda", "Seg"),
+          ("Terça", "Ter"),
+          ("Quarta", "Qua"),
+          ("Quinta", "Qui"),
+          ("Sexta", "Sex"),
+          ("Sábado", "Sáb")
+        ],
+      months =
+        [ ("Janeiro", "Jan"),
+          ("Fevereiro", "Fev"),
+          ("Março", "Mar"),
+          ("Abril", "Abr"),
+          ("Maio", "Mai"),
+          ("Junho", "Jun"),
+          ("Julho", "Jul"),
+          ("Agosto", "Ago"),
+          ("Setembro", "Set"),
+          ("Outubro", "Out"),
+          ("Novembro", "Nov"),
+          ("Dezembro", "Dez")
+        ],
+      amPm = ("am", "pm"),
+      dateTimeFmt = "%a %b %e %H:%M:%S %Z %Y",
+      dateFmt = "%d/%m/%y",
+      timeFmt = "%H:%M:%S",
+      time12Fmt = "%I:%M:%S %p",
+      knownTimeZones = []
+    }
